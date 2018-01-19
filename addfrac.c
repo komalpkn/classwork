7b48abfe50990c1e9e9fb7bceda5e3f78d87b31c
Komalpreet Kaur Nandra <komalpreetkaurnandra@Komalpreets-MacBook-Air.local>
2018-01-19 16:35:35 -0700

    practice code from lecture 1

--- addfrac.c
+++ addfrac.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
  int main(void){
-  int num1, denom1, num2, denom2, result_num, result_denom;
+  int num1, denom1, num2, denom2, result_num, result_denom, result_frac;
   
   printf("Enter first fraction: ");
   scanf("%d/%d", &num1, &denom1);
@@ -10,8 +10,9 @@
 
   result_num = (num1*denom2) + (num2*denom1);
   result_denom  = denom1*denom2;
+  result_frac = result_num/result_denom;
 
-  printf("The sum is %d/%d\n", result_num, result_denom);
+  printf("The sum is %f:\n", result_frac);
   
   return 0;
  } 
