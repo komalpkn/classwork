f17addd9f90586c0be6df3a2304b4427a4388610
Komalpreet Kaur Nandra <komalpreetkaurnandra@Komalpreets-MacBook-Air.local>
2018-01-19 21:58:45 -0700

    trying code from week 1

---
+++ celsius.c
@@ -0,0 +1,17 @@
+/*Convert farenheit to celsius*/
+
+#include <stdio.h>
+
+#define FREEZING_PT 32.0
+#define SCALE_FACTOR (5.0f/9.0f)
+
+int main(void){
+  float farenheit, celsius;
+  print("Enter farenheit temo: ");
+  scanf("%f", &farenheit);
+
+  celsius = (farenheit - FREEZING_PT) * SCALE_FACTOR;
+  
+  printf("Celsius equivalent: %0.1f\n", celsius);
+  return 0;
+}
