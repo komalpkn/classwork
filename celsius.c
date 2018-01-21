286ceafbd436ea11cd14f06395fc06684582be37
Komalpreet Kaur Nandra <komalpreetkaurnandra@Komalpreets-MacBook-Air.local>
2018-01-21 00:14:41 -0700

    now opens a file to read a farenheit temp and outputs a file with its celsius conversion

--- celsius.c
+++ celsius.c
@@ -6,12 +6,20 @@
 #define SCALE_FACTOR (5.0f/9.0f)
 
 int main(void){
+  FILE* fp_in1 = fopen("farenheitTemp.txt", "r");
+  FILE* fp_out1 = fopen("celsiusTemp.txt", "w");
   float farenheit, celsius;
-  printf("Enter farenheit temo: ");
-  scanf("%f", &farenheit);
-
+  
+  fscanf(fp_in1, "%f", &farenheit);
+  printf("Enter farenheit temp: %0.1f\n", farenheit);
+ 
   celsius = (farenheit - FREEZING_PT) * SCALE_FACTOR;
   
   printf("Celsius equivalent: %0.1f\n", celsius);
+  fprintf(fp_out1, "%0.1f\n", celsius);
+   
+  fclose(fp_in1);
+  fclose(fp_out1);
+
   return 0;
 }
